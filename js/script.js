fd78b2029eaee876adeae86ece3fe8ce71c14fe6
Sylwia <sylwiaszymanska2306@gmail.com>
2024-09-04 21:24:31 +0200

    added about us

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const nav = document.querySelector('.navbar')
 
     function addShadow(){
-        if(window.scrollY >=300){
+        if(window.scrollY >=100){
             nav.classList.add('shadow-bg')
         } else {
             nav.classList.remove('shadow-bg')
