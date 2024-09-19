49d5ca01df81066179cbafaf9b8dfb817be43f92
Sylwia <sylwiaszymanska2306@gmail.com>
2024-09-19 14:34:37 +0200

    added section team and section accomplishments

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-document.addEventListener('DOMContentLoaded', function(){
-    const nav = document.querySelector('.navbar')
-    const allNavItems = document.querySelectorAll('.nav-link')
-    const navList = document.querySelector('.navbar-collapse')
+document.addEventListener('DOMContentLoaded', function () {
+	const nav = document.querySelector('.navbar');
+	const allNavItems = document.querySelectorAll('.nav-link');
+	const navList = document.querySelector('.navbar-collapse');
 
-    function addShadow(){
-        if(window.scrollY >=100){
-            nav.classList.add('shadow-bg')
-        } else {
-            nav.classList.remove('shadow-bg')
-        }
-    }
+	function addShadow() {
+		if (window.scrollY >= 100) {
+			nav.classList.add('shadow-bg');
+		} else {
+			nav.classList.remove('shadow-bg');
+		}
+	}
 
-    allNavItems.forEach(item => item.addEventListener('click', () => {
-        
-    }))
+	allNavItems.forEach((item) => item.addEventListener('click', () => {}));
 
-    window.addEventListener('scroll',addShadow)
-})
+	window.addEventListener('scroll', addShadow);
+});
